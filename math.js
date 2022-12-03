31c7ffb28371176c4f4a2d6989114a3a42380def
Allen Nguyen <anhn321@gmail.com>
2022-12-02 16:19:10 -0800

    Populate display when number buttons clicked

--- math.js
+++ math.js
@@ -28,3 +28,26 @@ function operate(operator, num1, num2) {
 			return TypeError;
 	}
 }
+
+let output = document.querySelector(".output");
+output.textContent = "";
+let num1 = 0;
+
+const numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach((button) =>
+	button.addEventListener("click", () => {
+		output.textContent += button.textContent;
+		console.log(typeof button.textContent);
+		num1 = parseInt(output.textContent);
+		console.log(num1, typeof num1);
+	})
+);
+
+function clearOutput() {
+	const clear = document.querySelector(".clear");
+	clear.addEventListener("click", () => {
+		output.textContent = "";
+	});
+}
+
+clearOutput();
