02accda58139a70752d36ec4cee7c1b138df7ef7
Allen Nguyen <anhn321@gmail.com>
2022-12-02 17:09:54 -0800

    Store num1,  num2, and operator in global variables

--- math.js
+++ math.js
@@ -16,13 +16,13 @@ function divide(num1, num2) {
 
 function operate(operator, num1, num2) {
 	switch (operator) {
-		case "+":
+		case "add":
 			add(num1, num2);
-		case "-":
+		case "subtract":
 			subtract(num1, num2);
-		case "*":
+		case "multiply":
 			multiply(num1, num2);
-		case "/":
+		case "divide":
 			divide(num1, num2);
 		default:
 			return TypeError;
@@ -31,23 +31,49 @@ function operate(operator, num1, num2) {
 
 let output = document.querySelector(".output");
 output.textContent = "";
-let num1 = 0;
+let num1 = null;
+let operator = null;
+let num2 = null;
 
-const numberButtons = document.querySelectorAll(".number");
-numberButtons.forEach((button) =>
-	button.addEventListener("click", () => {
-		output.textContent += button.textContent;
-		console.log(typeof button.textContent);
-		num1 = parseInt(output.textContent);
-		console.log(num1, typeof num1);
-	})
-);
+function displayOutput() {
+	const numberButtons = document.querySelectorAll(".number");
+	numberButtons.forEach((button) =>
+		button.addEventListener("click", () => {
+			output.textContent += button.textContent;
+			if (operator === null) {
+				num1 = parseInt(output.textContent);
+			} else {
+				num2 = parseInt(output.textContent);
+			}
+			console.log("number1", num1, typeof num1);
+			console.log("number2", num2, typeof num2);
+			console.log("operator", operator, typeof operator);
+		})
+	);
+}
+
+function getOperator() {
+	const operatorButtons = document.querySelectorAll(".operator");
+	operatorButtons.forEach((button) =>
+		button.addEventListener("click", () => {
+			num1 = parseInt(output.textContent);
+			operator = button.classList[0];
+			output.textContent = null;
+			console.log(operator, typeof operator);
+		})
+	);
+}
 
 function clearOutput() {
 	const clear = document.querySelector(".clear");
 	clear.addEventListener("click", () => {
-		output.textContent = "";
+		output.textContent = null;
+		operator = null;
+		num1 = null;
+		num2 = null;
 	});
 }
 
+displayOutput();
 clearOutput();
+getOperator();
